2aee18b8afe32ae05f4ae313568f3817edfcf960
DESKTOP-NCK2EPM\ironMax <tothinktodo@163.com>
2023-06-03 19:05:23 +0800

    动态路由

--- pages/home-[id].vue
+++ pages/home-[id].vue
@@ -1,12 +1,13 @@
 <script>
 import { reactive, toRefs } from "vue";
-
+import { useRoute } from "~/.nuxt/vue-router";
 export default {
   setup() {
     const state = reactive({
       count: 0,
     });
-
+    let route = useRoute();
+    console.log(route.params.id, "id");
     return {
       ...toRefs(state),
     };
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>index</div>
+  <NuxtLink to="/home-123"> to home </NuxtLink>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
