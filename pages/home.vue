355231dbac4bc61e117bda7785568184ba04bf04
DESKTOP-NCK2EPM\ironMax <tothinktodo@163.com>
2023-06-03 18:39:42 +0800

    vuessr

---
+++ pages/home.vue
@@ -0,0 +1,19 @@
+<script>
+import { reactive, toRefs } from "vue";
+
+export default {
+  setup() {
+    const state = reactive({
+      count: 0,
+    });
+
+    return {
+      ...toRefs(state),
+    };
+  },
+};
+</script>
+
+<template>
+  <div>home</div>
+</template>
